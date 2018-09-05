0b28ed06370e95dbe808cefbe93e6aaa9c40f105
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-05 15:55:38 +0200

    Added new page to show a specific post that is based on post ID

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 export const FETCH_POSTS = 'fetch_posts'
+export const FETCH_POST = 'fetch_post'
 export const CREATE_POST = 'create_post'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
@@ -14,7 +15,6 @@ export function fetchPosts() {
     }
 }
 
-
 export function createPost(values, callback) {
     const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
         .then(() => callback())
@@ -24,4 +24,13 @@ export function createPost(values, callback) {
         payload: request
     }
 
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    }
 }
